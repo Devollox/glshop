4729e245a38bf1825f7ce4eac37b97f0542ecad1
Devollox <122895078+Devollox@users.noreply.github.com>
2024-10-25 00:43:48 +0300

    Delete components directory

--- components/icons/settings.tsx
+++
@@ -1,6 +0,0 @@
-import Icon from "../icon"
-
-const Settings = `<path d="M1 7H13M1 11.5H13M1 16H13" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>`
-
-export default Icon(Settings)
-
