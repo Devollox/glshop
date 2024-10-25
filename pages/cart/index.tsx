52b11d64eb2fef33a1d9f84fa88be03b21ce5e73
Devollox <122895078+Devollox@users.noreply.github.com>
2024-10-25 22:02:54 +0300

    Update index.tsx

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,4 +1,3 @@
-// Cart.tsx
 import React, { useEffect, useState } from "react";
 import TapBar from "@/components/tapbar";
 import Page from "@/components/page";
@@ -6,44 +5,44 @@ import MainContent from "@/components/maincontent";
 import { cartService, CartItem } from "@/pages/cart/cartservice";
 
 const Cart: React.FC = () => {
-  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+    const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
-  useEffect(() => {
-    const subscription = cartService.cartItems$.subscribe((items) => {
-      setCartItems(items);
-    });
+    useEffect(() => {
+        const subscription = cartService.cartItems$.subscribe((items) => {
+            setCartItems(items);
+        });
 
-    return () => {
-      subscription.unsubscribe();
+        return () => {
+            subscription.unsubscribe();
+        };
+    }, []);
+
+    const handleRemove = (itemToRemove: CartItem) => {
+        cartService.removeFromCart(itemToRemove);
     };
-  }, []);
 
-  const handleRemove = (itemToRemove: CartItem) => {
-    cartService.removeFromCart(itemToRemove);
-  };
-
-  return (
-    <>
-      <Page title={"Cart"}>
-        <MainContent height={"100%"} margin={'100px 0 0 0'}>
-            <div>
-              {cartItems.length === 0 ? (
-                <p>Корзина пуста</p>
-              ) : (
-                cartItems.map((item) => (
-                  <div key={item.slug} >
-                    <span>{item.name}</span>
-                    <span>{item.price} руб.</span>
-                    <button onClick={() => handleRemove(item)}>Удалить</button>
-                  </div>
-                ))
-              )}
-            </div>
-        </MainContent>
-        <TapBar catalog={".5"} cart={"1"} main={".5"}/>
-      </Page>
-    </>
-);
-}
+    return (
+        <>
+            <Page title="Cart">
+                <MainContent height="100%" margin="100px 0 0 0">
+                    <div>
+                        {cartItems.length === 0 ? (
+                            <p>Корзина пуста</p>
+                        ) : (
+                            cartItems.map((item) => (
+                                <div key={item.slug}>
+                                    <span>{item.name}</span>
+                                    <span>{item.price} руб.</span>
+                                    <button onClick={() => handleRemove(item)}>Удалить</button>
+                                </div>
+                            ))
+                        )}
+                    </div>
+                </MainContent>
+                <TapBar catalog=".5" cart="1" main=".5" />
+            </Page>
+        </>
+    );
+};
 
 export default Cart;
